d5fb9547c9f757c2a16cd982fdbef77345e089f7
Dungmt86 <dungmt86@gmail.com>
2022-04-04 18:11:51 +0700

    test nop bai

---
+++ so chan so le.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(){
+	
+ //input num
+  int num;
+  scanf("%d",&num);
+  
+  // r=num % 2
+  int r;
+  r=num%2;
+  
+  // kiem tra r==0
+  if(r==0){
+  	// yes
+  	printf("num la so chan");
+  } else {
+  	// no
+  	printf("num la so le");
+  	
+  }
+// stop
+}
