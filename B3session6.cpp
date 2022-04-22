4195f65737b074e7618cb613785dc7c87c030aab
Dungmt86 <dungmt86@gmail.com>
2022-04-22 15:39:44 +0700

    bai ve nha session 6

---
+++ B3session6.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(){
+	int n;
+	printf("NHap n=");
+	scanf("%d",&n);
+	int arr[n];
+	printf("Nhap mang:");
+	for(int i=0;i<n;i++){
+		scanf("%d",&arr[i]);
+	}
+
+	int a = arr[0];
+	int b = arr[0];
+	for(int i=1;i<n;i++){
+		if(arr[i]> b){
+			b = arr[i];
+		}
+		if(arr[i] < a){
+			a = arr[i];
+		}
+	}
+	printf("Doan can tim [%d,%d]",a,b);
+}
